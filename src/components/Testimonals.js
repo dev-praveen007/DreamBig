86b990a6dc573f1818f44c7d4c20758fcee3de6a
dev-praveen007 <theripraveen2002@gmail.com>
2024-07-16 08:11:23 +0530

    animation

--- src/components/Testimonals.js
+++ src/components/Testimonals.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Carousel } from "react-bootstrap";
 import image2 from "../assests/images/jkskfd.webp";
 import image1 from "../assests/images/Candid Photography.webp";
@@ -33,59 +33,60 @@ function Testimonals() {
     },
   ];
 
+  const [activeIndex,setActiveIndex]=useState();
+
+  const handleSelect = (selectedIndex, e) => {
+    console.log('Slide started',selectedIndex);
+    setActiveIndex(selectedIndex)
+  };
 
   return (
     <div className="pt-5 pb-5 testimon-full-cont">
-      <h1 className="text-center pb-5" data-aos="fade-up" >Testimonials</h1>
-          <div className="d-flex  justify-content-center align-items-center"  data-aos="fade-up"  >
+      <h1 className="text-center pb-5" data-aos="fade-up">Testimonials</h1>
+      <div className="d-flex  justify-content-center align-items-center" data-aos="fade-up">
         <Carousel
           fade
           className="w-100 d-flex justify-content-center align-items-center mb-5"
           indicators={false}
+          touch
+          pause={false}
+          onSelect={handleSelect}
         >
-            {arr.map((val,i)=>(
-               i % 2 !== 0 ?
-                <Carousel.Item
-                  className="w-100 d-flex justify-content-center align-items-center gap-1"
-                  interval={2000}
-                >
-                  <div className="testimonals-text-container ">
-                    <div className="w-75">
-                      <p className="text-center fs-10">
-                        {val.desc}
-                      </p>
-                      <p className="text-center mb-0 fs-14 fw-600">
-                        - {val.name}
-                      </p>
-                      <p className="text-center fs-13 fw-600">
-                        {val.from}
-                      </p>
-                    </div>
+          {arr.map((val, i) =>
+            i % 2 !== 0 ? (
+              <Carousel.Item
+                className="w-100 d-flex justify-content-center align-items-center"
+                interval={3000}
+              >
+                <div className={`testimonals-text-container ${activeIndex === i ? "opacity-animation" : ""}`}>
+                  <div className="w-75">
+                    <p className="text-center fs-10">{val.desc}</p>
+                    <p className="text-center mb-0 fs-14 fw-600">
+                      - {val.name}
+                    </p>
+                    <p className="text-center fs-13 fw-600">{val.from}</p>
                   </div>
-                  <img alt="FirstImg" className="testimo-img " src={val.image} />
-                </Carousel.Item>
-                :
-                <Carousel.Item
-                  className="w-100 d-flex justify-content-center align-items-center gap-1"
-                  interval={2000}
-                >
-                  <img alt="FirstImg" className="testimo-img " src={val.image} />
-                  <div className="testimonals-text-container ">
-                    <div className="w-75">
-                      <p className="text-center fs-10">
-                        {val.desc}
-                      </p>
-                      <p className="text-center mb-0 fs-14 fw-600">
-                        - {val.name}
-                      </p>
-                      <p className="text-center fs-13 fw-600">
-                        {val.from}
-                      </p>
-                    </div>
+                </div>
+                <img alt="FirstImg" className={`testimo-img  ${activeIndex === i ? "testi-animation-left" :""}`} src={val.image} />
+              </Carousel.Item>
+            ) : (
+              <Carousel.Item
+                className="w-100 d-flex justify-content-center align-items-center "
+                interval={3000}
+              >
+                <img alt="FirstImg" className={`testimo-img ${(activeIndex === i || activeIndex === 0) ? "testi-animation-rightt":""}`} src={val.image}/>
+                <div className={`testimonals-text-container ${(activeIndex === i || activeIndex === 0) ? "opacity-animation":""}`} >
+                  <div className="w-75">
+                    <p className="text-center fs-10">{val.desc}</p>
+                    <p className="text-center mb-0 fs-14 fw-600">
+                      - {val.name}
+                    </p>
+                    <p className="text-center fs-13 fw-600">{val.from}</p>
                   </div>
-                </Carousel.Item>
-            ))}
-          
+                </div>
+              </Carousel.Item>
+            )
+          )}
         </Carousel>
       </div>
     </div>
